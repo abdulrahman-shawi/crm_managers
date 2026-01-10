deaf6601f33e8fc1de6cb678087dabc99802391f
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-01-10 18:14:51 +0300

    s

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -106,7 +106,7 @@ export default function InvoicesPage() {
             </div>
 
             {/* الجدول */}
-            <div className="">g</div>
+            <div className="bg-red-400 text-yellow-400">g</div>
             <InvoicesTable invoices={invoices} />
 
             <AddInvoiceModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} manager={invoices} type={activeTab} customers={customers} products={products} />
